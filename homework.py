e2e195c77e127834eb4f94bbafd23ab0ba72d2cd
Sergey Kuroedov <sergeylive87@yandex.ru>
2024-01-29 17:36:28 +0300

    Homework over errors

--- homework.py
+++ homework.py
@@ -14,6 +14,7 @@ from exceptions import (
     SendRequestError,
     StatusCodeError,
 )
+from http import HTTPStatus
 from telegram.error import BadRequest, Unauthorized
 
 load_dotenv()
@@ -22,8 +23,6 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-LIMIM_ERRORS = 3
-EXPECTED_CODE = 200
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -46,11 +45,11 @@ logger.addHandler(handler)
 
 def check_tokens():
     """Global variable should musts exist and not be NONE."""
-    required_variable = [
+    required_variable = (
         'TELEGRAM_CHAT_ID',
         'TELEGRAM_TOKEN',
         'PRACTICUM_TOKEN',
-    ]
+    )
     global_variable = globals()
     phrase = 'Not required variable: {}'
     for variable in required_variable:
@@ -78,41 +77,31 @@ def send_message(bot, message):
 def get_api_answer(timestamp):
     """Request to YandexPracticum Homework."""
     try:
+        logger.info(
+            f'Send request to YaHomework API. Time: {time.ctime(timestamp)}'
+        )
         response = requests.get(
             url=ENDPOINT,
             headers=HEADERS,
             params={'from_date': timestamp}
         )
-        if response.status_code != EXPECTED_CODE:
+        if response.status_code != HTTPStatus.OK:
             raise StatusCodeError('Status code different to expected')
-        logger.info(
-            f'Send request to YaHomework API. Time: {time.ctime(timestamp)}'
-        )
         return response.json()
     except requests.RequestException as err:
         raise RequestError('Problem with Request') from err
 
 
 '''
-Пробовал добавить в check_response код ниже, но тесты ломаются.
-Даже если местами поменять, то что уже написано, тоже тесты ломаются
-
-   asdf = (
-        (response, dict),
-        (response['homeworks'], list),
-        (response['current_date'], int),
-    )
-    for a, b in asdf:
-        if not isinstance(a, b):
-            raise TypeError(
-                f'Received: "{type(a)}". Expected: "{b}"'
-            )
+Тесты против:
+AssertionError: Функция `check_response` должна принимать
+количество аргументов: 1
 '''
 
 
 def check_response(response):
     """Is correct answer to API YandexPracticum."""
-    important_keys = ['homeworks', 'current_date']
+    important_keys = ('homeworks', 'current_date')
 
     if not isinstance(response, dict):
         raise TypeError(
@@ -147,31 +136,38 @@ def parse_status(homework):
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
+def handler_errors(stack, error, count_err=3):
+    """Three (default) identical errors lead to exit from system."""
+    stack.append(error.__repr__())
+    if len(stack) == count_err:
+        if all(err == stack[0] for err in stack):
+            sys.exit('Error in works Bot')
+        else:
+            del stack[0:2]
+
+
 def main():
     """Base logic Bot."""
     check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     message_storage = ''
-    allowed_errors = 0
+    error_stack = []
     while True:
         try:
             response = get_api_answer(timestamp)
             check_response(response)
             homeworks = response.get('homeworks')
-            time_server = response.get('current_date', timestamp)
-            timestamp = time_server
+            timestamp = response.get('current_date', timestamp)
             if homeworks:
                 homework = homeworks[0]
                 message = parse_status(homework)
                 if message_storage != message:
                     send_message(bot, message)
                     message_storage = message
-        except BotError as err:
+        except (AuthorizationError, SendRequestError) as err:
             logger.critical(err)
-            if allowed_errors == LIMIM_ERRORS:
-                sys.exit('Error in works Bot')
-            allowed_errors += 1
+            handler_errors(error_stack, err)
         except Exception as error:
             message_err = f'Сбой в работе программы: {error}'
             logger.error(error)
